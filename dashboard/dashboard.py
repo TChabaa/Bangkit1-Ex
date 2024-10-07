056116a17fc9cec23643b4c88862f667f0543caa
TChabaa <bhirawa24082004@gmail.com>
2024-10-07 16:33:06 +0700

    new rev 2

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,26 +7,8 @@ from babel.numbers import format_currency
 sns.set(style='dark')
 
 # Load datasets
-day_df = pd.read_csv("../data/day.csv")
-hour_df = pd.read_csv("../data/hour.csv")
-
-datachange = [day_df, hour_df]
-
-for column in datachange:
-    column['dteday'] = pd.to_datetime(column['dteday'])
-
-day_df.info()
-print("Jumlah Duplikasi", day_df.duplicated().sum())
-
-hour_df.info()
-print("Jumlah Duplikasi", hour_df.duplicated().sum())
-
-print(day_df.describe(include=[np.number]))
-print(hour_df.describe(include=[np.number]))
-
-# Scale the temperature
-day_df['atemp_scaled'] = day_df['atemp'] * 50
-
+day_df = pd.read_csv("day_use.csv")
+hour_df = pd.read_csv("hour_use.csv")
 # Helper functions to create grouped DataFrames
 def create_by_season_casual_df(df):
     return df.groupby(by='season').casual.mean().reset_index()
---
+++ dashboard/exploritary.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+# Load datasets
+day_df = pd.read_csv("../data/day.csv")
+hour_df = pd.read_csv("../data/hour.csv")
+
+datachange = [day_df, hour_df]
+
+for column in datachange:
+    column['dteday'] = pd.to_datetime(column['dteday'])
+
+day_df.info()
+print("Jumlah Duplikasi", day_df.duplicated().sum())
+
+hour_df.info()
+print("Jumlah Duplikasi", hour_df.duplicated().sum())
+
+print(day_df.describe(include=[np.number]))
+print(hour_df.describe(include=[np.number]))
+
+# Scale the temperature
+day_df['atemp_scaled'] = day_df['atemp'] * 50
+
+day_df.to_csv("../dashboard/day_use.csv")
+hour_df.to_csv("../dashboard/hour_use.csv")
